590f4add49f35b7a333bf85dd97351319450af91
Ben <88286087+BenjaminLentini@users.noreply.github.com>
2024-07-09 08:18:30 -0700

    Update lsiget.sh

--- lsiget.sh
+++ lsiget.sh
@@ -1,1 +1,1 @@
-cd && sudo wget https://github.com/BenjaminLentini/LSI-Get/raw/main/lsiget_linux_021323.tgz && tar zxvf lsiget_linux_021323.tgz && sudo bash ./lsigetlinux_021323.sh -D -Q
+cd ~ && sudo wget https://github.com/BenjaminLentini/LSI-Get/raw/main/lsiget_linux_021323.tgz && tar zxvf lsiget_linux_021323.tgz && sudo bash ./lsigetlinux_021323.sh -D -Q
